6bc5969e62982747acf52e8ee2d492188c1d5d58
ewasion137 <formanban94@gmail.com>
2026-05-18 10:40:59 +0300

    Mutations in mutations.lua

---
+++ src/logic/mutations.lua
@@ -0,0 +1,10 @@
+local mutations = {
+    { id = "none", name = "None", multiplier = 1.0, chance = 93.495 },
+    { id = "glowing", name = "Glowing", multiplier = 1.02, chance = 2.0 },
+    { id = "scorching", name = "Scorching", multiplier = 1.1, chance = 2.0 },
+    { id = "iridescent", name = "Iridescent", multiplier = 1.3, chance = 1.5 },
+    { id = "radioactive", name = "Radioactive", multiplier = 2.0, chance = 1.0 },
+    { id = "prismatic", name = "Prismatic", multiplier = 5.0, chance = 0.05 }
+}
+
+return mutations
